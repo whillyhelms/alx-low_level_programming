cfcb1476d3390c23cd9ce7a0883a1742cede3b25
whillyhelms <whillyhelms@gmail.com>
2022-12-11 11:19:59 -0800

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *
+ * main - Entry point
+ * int main (void)
+ * return: Always (0)
+ */
+
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	Return: (0);
+
+}
