0d4f739873e5bbebf1b0939e0d2934a21c8dc369
whillyhelms <whillyhelms@gmail.com>
2022-12-11 07:05:25 -0800

    Programming is like a puzzle

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,11 +5,10 @@
  *
  * Return: Always 0 (success) 
  */
-
 int main (void)
+
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
-	
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 
 }
